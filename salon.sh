e2e207a372ce1a2408d765db3bd9c8be51e37f9b
CodeSumner <117532559+CodeSumner@users.noreply.github.com>
2023-09-21 12:00:47 -0700

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -9,8 +9,8 @@ CUSTOMER_NAME=""
 CUSTOMER_PHONE=""
 
 MAKE_APPOINTMENT() {
-    SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
-    CUSTOMERS=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+    SERVICE_NAME=$($PSQL "SELECT service_name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+    CUSTOMERS=$($PSQL "SELECT customer_name FROM customers WHERE phone='$CUSTOMER_PHONE'")
     CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
     SERVICE_NAME_FORMATTED=$(echo $SERVICE_NAME | sed 's/ //g')
     CUSTOMERS_FORMATTED=$(echo $CUSTOMERS | sed 's/ //g')
@@ -26,7 +26,7 @@ MAIN_MENU(){
       echo -e "\n$1"
   fi
 
-  SERVICES=$($PSQL "SELECT service_id, name FROM services")
+  SERVICES=$($PSQL "SELECT service_id, service_name FROM services")
 
   echo "$SERVICES" | while read SERVICE_ID BAR NAME
   do
@@ -45,13 +45,13 @@ MAIN_MENU(){
           echo -e "\nWhat's your phone number?"
           read CUSTOMER_PHONE
 
-          REGULAR_CUSTOMER=$($PSQL "SELECT customer_id, name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+          REGULAR_CUSTOMER=$($PSQL "SELECT customer_id, customer_name FROM customers WHERE phone='$CUSTOMER_PHONE'")
 
           if [[ -z $REGULAR_CUSTOMER ]]
           then
             echo -e "\nI don't have a record for that phone number, what's your name?"
             read CUSTOMER_NAME
-            INSERTED_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+            INSERTED_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(customer_name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
             MAKE_APPOINTMENT
             else
             MAKE_APPOINTMENT
